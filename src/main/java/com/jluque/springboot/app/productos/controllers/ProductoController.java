7e8ee4a66c9d818735eeb6fc3d668cf86f07d7ba
julio.a.luque <julio.a.luque@gmail.com>
2020-09-27 03:43:01 -0300

    PutMapping

--- src/main/java/com/jluque/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/jluque/springboot/app/productos/controllers/ProductoController.java
@@ -1,5 +1,6 @@
 package com.jluque.springboot.app.productos.controllers;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +64,19 @@ public class ProductoController {
 		return productoService.save(producto);
 	}
 
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoDb = productoService.findById(id);
+		productoDb.setNombre(producto.getNombre());
+		productoDb.setPrecio(producto.getPrecio());
+		productoDb.setCreateAt(new Date());
+
+		return productoService.save(productoDb);
+	}
+
 	@DeleteMapping("/borrar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void borrar(@PathVariable Long id) {
 		productoService.deleteById(id);
 	}
