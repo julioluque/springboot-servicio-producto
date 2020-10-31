157babd5a8b399b750d07aa79f870a2639e6acc0
julio.a.luque <julio.a.luque@gmail.com>
2020-10-30 21:49:33 -0300

    incluimos commons en producto

--- src/main/java/com/jluque/springboot/app/productos/SpringbootServicioProductoApplication.java
+++ src/main/java/com/jluque/springboot/app/productos/SpringbootServicioProductoApplication.java
@@ -2,10 +2,12 @@ package com.jluque.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EntityScan({ "com.jluque.springboot.app.commons.models.entity" }) // incluimos dentro del contexto
 public class SpringbootServicioProductoApplication {
 
 	public static void main(String[] args) {
